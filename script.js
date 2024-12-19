b74dc9f14aecbf85da5a1821a3946cc5d861df93
oliver36-y <olivera21123@gmail.com>
2024-12-19 14:43:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 for (var i = 0; i < 20; i++) {
-  var newTab = window.open('https://olivera36-y.github.io/hom3/main', '_blank');
+  var newTab = window.open('https://oliver36-y.github.io/hom3/main/index.HTML', '_blank');
   newTab.blur(); // remove focus from the new tab
   window.focus(); // keep the current window focused
   setTimeout(function() {}, 1000); // wait for 1 second before opening the next tab
